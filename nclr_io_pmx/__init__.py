b33f4a6f20738f2f0e9412f6a09d444d9e365c46
LNSEAB <lnseab@gmail.com>
2014-09-03 08:35:52 +0900

    __init__.py追加

---
+++ nclr_io_pmx/__init__.py
@@ -0,0 +1,91 @@
+# coding = utf-8
+
+# nclr PMX Exporter for Blender
+# Copyright LNSEAB 2014
+
+bl_info = {
+    "name" : "PMX Format IO",
+    "author" : "LNSEAB",
+    "version" : ( 0, 1 ),
+    "Blender" : ( 2, 71, 0 ),
+    "location" : "File > Import-Export",
+    "description" : "Import-Export PMX mesh, UV's and materials",
+    "warning" : "",
+    "wiki_url" : "",
+    "tracker_url" : "",
+    "category" : "Import-Export"
+}
+
+import bpy
+import bpy_extras
+from . import export_pmx
+
+class pmx_exporter(bpy.types.Operator, bpy_extras.io_utils.ExportHelper) :
+    """Save a PMX File"""
+
+    bl_idname = "exporter.pmx"
+    bl_label = "Export PMX Format Data"
+
+    filename_ext = ".pmx"
+    filter_glob = bpy.props.StringProperty( default = "*.pmx", options = { "HIDDEN" } )
+
+    encoding = bpy.props.EnumProperty(
+        name = "Select Encoding",
+        items = (
+            ( "utf-8", "UTF-8", "" ),
+            ( "utf-16", "UTF-16(LE)", "" )
+        ),
+        description = "",
+        default = "utf-16"
+    )
+
+    write_objects_type = bpy.props.EnumProperty(
+        name = "Write Object",
+        items = (
+            ( "visible", "Visible Only", "" ),
+            ( "selection", "Selection Only", "" ),
+            ( "all", "All", "" )
+        ),
+        description = "",
+        default = "all"
+    )
+
+    path_type = bpy.props.EnumProperty(
+        name = "Path",
+        items = (
+            ( "abs", "Absolute Path", "" ),
+            ( "rel", "Relative Path", "" )
+        ),
+        description = "",
+        default = "rel"
+    )
+
+    apply_modifiers = bpy.props.BoolProperty(
+        name = "Apply Modifiers",
+        description = "Apply Modifiers (preview resolution)",
+        default = True
+    )
+
+    def execute(self, context) :
+        params = {
+            "filepath" : self.filepath,
+            "encoding" : self.encoding,
+            "write_objects_type" : self.write_objects_type,
+            "path_type" : self.path_type,
+            "apply_modifiers" : self.apply_modifiers
+        }
+        return export_pmx.save( params )
+
+def menu_func_export(self, context) :
+    self.layout.operator( pmx_exporter.bl_idname, text = "PMX (.pmx)" )
+
+def register() :
+    bpy.utils.register_module( __name__ )
+    bpy.types.INFO_MT_file_export.append( menu_func_export )
+
+def unregister() :
+    bpy.utils.unregister_module( __name__ )
+    bpy.types.INFO_MT_file_export.remove( menu_func_export )
+
+if __name__ == "__main__" :
+    register()
